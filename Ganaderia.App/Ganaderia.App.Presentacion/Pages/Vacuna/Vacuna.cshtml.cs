772856d2676470b0764c5cce04da30e6c2b14876
alejos17 <alejos17@gmail.com>
2021-10-08 16:26:18 -0500

    Backend - Lista Vacunas

--- Ganaderia.App/Ganaderia.App.Presentacion/Pages/Vacuna/Vacuna.cshtml.cs
+++ Ganaderia.App/Ganaderia.App.Presentacion/Pages/Vacuna/Vacuna.cshtml.cs
@@ -4,13 +4,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Ganaderia.App.Dominio;
+using Ganaderia.App.Persistencia;
 
 namespace Ganaderia.App.Presentacion.Pages
 {
     public class VacunaModel : PageModel
     {
+        private static IRepositorioVacuna _repoVacuna = new RepositorioVacuna(new Persistencia.AppContext());
+
+        public IEnumerable<Vacuna> vacunas { get; private set; }
+        
         public void OnGet()
         {
+            vacunas = _repoVacuna.GetAllVacunas();
         }
     }
 }
