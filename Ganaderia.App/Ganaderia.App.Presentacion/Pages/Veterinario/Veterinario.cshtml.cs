5f9252326c1e429b673a47fb1352f29c102532c4
alejos17 <alejos17@gmail.com>
2021-10-08 16:13:10 -0500

    Backend - Lista Veterinarios

--- Ganaderia.App/Ganaderia.App.Presentacion/Pages/Veterinario/Veterinario.cshtml.cs
+++ Ganaderia.App/Ganaderia.App.Presentacion/Pages/Veterinario/Veterinario.cshtml.cs
@@ -4,13 +4,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Ganaderia.App.Dominio;
+using Ganaderia.App.Persistencia;
 
 namespace Ganaderia.App.Presentacion.Pages
 {
     public class VeterinarioModel : PageModel
     {
+        private static IRepositorioVeterinario _repoVeterinario= new RepositorioVeterinario(new Persistencia.AppContext());
+        
+        public IEnumerable<Veterinario> veterinarios { get; private set; }
+
         public void OnGet()
         {
+            veterinarios = _repoVeterinario.GetAllVeterinarios();
         }
     }
 }
