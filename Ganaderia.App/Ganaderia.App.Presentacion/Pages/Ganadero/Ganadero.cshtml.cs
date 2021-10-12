b96c74be89eacba89056d42e531d7eb9ab4d393e
alejos17 <alejos17@gmail.com>
2021-10-12 14:26:18 -0500

    Cambios en Razor-AppContext Ganadero

--- Ganaderia.App/Ganaderia.App.Presentacion/Pages/Ganadero/Ganadero.cshtml.cs
+++ Ganaderia.App/Ganaderia.App.Presentacion/Pages/Ganadero/Ganadero.cshtml.cs
@@ -12,21 +12,38 @@ namespace Ganaderia.App.Presentacion.Pages
     public class GanaderoModel : PageModel
     {
         private static IRepositorioGanadero _repoGanadero= new RepositorioGanadero(new Persistencia.AppContext());
-
+        [BindProperty]  //Enlazar el modelo con el input del HTML
         public Ganadero ganadero { get; set; }
+
+        public int ganaderoId { get; set; }
         
         
         
         public IActionResult OnGet(int ganaderoId)
         {
-            ganadero = _repoGanadero.GetGanadero(ganaderoId);
-            if(ganadero==null)
-            {
-                return RedirectToPage("./Error");
-            }else
+            if(ganaderoId==0987654321)
             {
                 return Page();
             }
+            else{
+                ganadero = _repoGanadero.GetGanadero(ganaderoId);
+                if(ganadero==null)
+                {
+                   return RedirectToPage("./Error");
+                }else
+                {
+                   return Page();
+                }
+            }
+            
         }
+
+        //Para editar valores de Ganadero
+        public IActionResult OnPost()
+        {
+            ganadero = _repoGanadero.UpdateGanadero(ganadero);
+            return Page();
+        }
+
     }
 }
