eb671af206d6b0cc32671a4bf5ebffb393e1e515
alejos17 <alejos17@gmail.com>
2021-10-12 13:38:45 -0500

    Controller Ganado, Cambios en Lista Ejemplar

--- Ganaderia.App/Ganaderia.App.Presentacion/Pages/Ejemplar/Lista.cshtml.cs
+++ Ganaderia.App/Ganaderia.App.Presentacion/Pages/Ejemplar/Lista.cshtml.cs
@@ -13,13 +13,21 @@ namespace Ganaderia.App.Presentacion.Pages
     {
         //Llamamos instancia del Repo Ejemplar para traer datos de la BD
         private static IRepositorioEjemplar _repoEjemplar= new RepositorioEjemplar(new Persistencia.AppContext());
+        private static IRepositorioGanado _repoGanado= new RepositorioGanado(new Persistencia.AppContext());
 
         //Lista de Ejemplares
         public IEnumerable<Ejemplar> ejemplares { get; private set; }
+
+        public IEnumerable<Ganado> ganado { get; private set; }
+        public int idGanado { get; private set; }
+
+        public int valor { get; private set; }
         
         public void OnGet(int idGanado)
         {
+            ganado = _repoGanado.GetAllGanado();
             ejemplares = _repoEjemplar.GetEjemplarxGanado(idGanado);
+            
         }
     }
 }
---
+++ Ganaderia.App/Ganaderia.App.Servicios/Controllers/GanadoController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Ganaderia.App.Dominio;
+using Ganaderia.App.Persistencia;
+
+namespace Ganaderia.App.Servicios.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GanadoController : ControllerBase
+    {
+        private static IRepositorioGanado _repoGanado= new RepositorioGanado(new Persistencia.AppContext());
+        private static IRepositorioEjemplar _repoEjemplar= new RepositorioEjemplar(new Persistencia.AppContext());
+
+        public IEnumerable<Ejemplar> ejemplares { get; private set; }
+
+        //Recepcion de informacion desde backend
+        [HttpGet]
+        public IEnumerable<Ganado> Get()
+        {
+            var ganado = _repoGanado.GetAllGanado();
+            return ganado;
+
+        }
+
+        //Enviar datos al backend
+        //Debe ser un unico POST por controllador que envie todo....
+        [HttpPost]
+        public IEnumerable<Ejemplar> Post(int idGanado)
+        {
+            ejemplares = _repoEjemplar.GetEjemplarxGanado(idGanado);
+            return ejemplares;
+        }
+    }
+}
